88f49de54c5bc8acae17b59f6ff9c99833cec60a
rcnr <iop04226@gmail.com>
2026-04-06 23:34:05 +0900

    refactor: RestTemplate에 커넥션 및 읽기 타임아웃 설정 추가

--- src/main/java/naughty/tuzamate/global/config/WebConfig.java
+++ src/main/java/naughty/tuzamate/global/config/WebConfig.java
@@ -5,11 +5,13 @@ import naughty.tuzamate.auth.resolver.UserIdInfoResolver;
 import naughty.tuzamate.auth.resolver.UserInfoResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -27,7 +29,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofMillis(2000));
+        factory.setReadTimeout(Duration.ofMillis(5000));
+        return new RestTemplate(factory);
     }
 
     @Override

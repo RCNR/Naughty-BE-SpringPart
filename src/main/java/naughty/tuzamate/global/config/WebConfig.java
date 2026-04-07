04816fe69b0c2f2a686d6dd718c5ec2a7a2f8397
rcnr <iop04226@gmail.com>
2026-04-07 11:50:09 +0900

    refactor: RestTemplate 설정을 HttpClient 기반으로 변경하여 커넥션 및 타임아웃 관리 개선

--- src/main/java/naughty/tuzamate/global/config/WebConfig.java
+++ src/main/java/naughty/tuzamate/global/config/WebConfig.java
@@ -3,15 +3,21 @@ package naughty.tuzamate.global.config;
 import lombok.RequiredArgsConstructor;
 import naughty.tuzamate.auth.resolver.UserIdInfoResolver;
 import naughty.tuzamate.auth.resolver.UserInfoResolver;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.DefaultHttpRequestRetryStrategy;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -29,9 +35,27 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofMillis(2000));
-        factory.setReadTimeout(Duration.ofMillis(5000));
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(20);
+        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setDefaultConnectionConfig(ConnectionConfig.custom()
+                .setConnectTimeout(Timeout.ofMilliseconds(5000))
+                .setSocketTimeout(Timeout.ofMilliseconds(5000))
+                .setValidateAfterInactivity(Timeout.ofMilliseconds(1000))
+                .build());
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setResponseTimeout(Timeout.ofMilliseconds(5000))
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .setRetryStrategy(new DefaultHttpRequestRetryStrategy(0, Timeout.ofMilliseconds(0)))
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
         return new RestTemplate(factory);
     }
 

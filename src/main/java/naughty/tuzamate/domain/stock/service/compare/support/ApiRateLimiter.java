c55d89e505bc375a7c0815552cdf257c97c0e94b
rcnr <iop04226@gmail.com>
2026-04-15 16:18:17 +0900

    refactor: ApiRateLimiter 인터페이스에 블로킹 및 리액티브 호출 제어 메서드 추가

--- src/main/java/naughty/tuzamate/domain/stock/service/compare/support/ApiRateLimiter.java
+++ src/main/java/naughty/tuzamate/domain/stock/service/compare/support/ApiRateLimiter.java
@@ -3,6 +3,9 @@ package naughty.tuzamate.domain.stock.service.compare.support;
 import reactor.core.publisher.Mono;
 
 public interface ApiRateLimiter {
+    // API 호출 전에 호출 빈도 제한을 적용하는 메서드
     void acquireBlocking() throws InterruptedException;
+
+    // API 호출 전에 호출 빈도 제한을 적용하는 메서드 (reactive 버전)
     Mono<Void> acquireReactive();
 }
